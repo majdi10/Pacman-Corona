078b93f7fdb165b8a3592d49b4bd30daa5a7ab4e
Eliko <yyyeliko@gmail.com>
2020-03-30 21:40:46 +0300

    Getting roads

--- eli.py
+++ eli.py
@@ -1,18 +1,25 @@
+import collections
+
 import cv2
-import numpy
+import numpy as np
 import random
 from matplotlib import pyplot as plt
 from tkinter import *
 from PIL import ImageTk, Image
 from tkinter import filedialog
+from scipy.spatial import distance
 
+CHOSE_PAINTED_PIXELS = 80
 painted_pixels = 0
 bfs_arr = []
 center = []
+centers = []
 min_height = 0
 max_height = 0
 min_width = 0
 max_width = 0
+LINE_COLOR = [100, 40, 255]
+COIN_RADIUS = 4
 
 
 def check_min_max_height_width(x, y):
@@ -27,62 +34,61 @@ def check_min_max_height_width(x, y):
         max_width = y
 
 
-def bfs(x, y, color):
+def bfs(passed_image, x, y, color):
     global painted_pixels, bfs_arr
-
-    if x + 1 < image.shape[0]:
-        if sum(image[x + 1][y]) == 0:  # if black
+    if x + 1 < passed_image.shape[0]:
+        if sum(passed_image[x + 1][y]) == 0:  # if black
             bfs_arr.insert(0, [x + 1, y, color])
-            image[x + 1][y] = color
+            passed_image[x + 1][y] = color
             painted_pixels -= 1
             check_min_max_height_width(x+1, y)
 
-    if y + 1 < image.shape[1]:
-        if sum(image[x][y + 1]) == 0:  # if black
+    if y + 1 < passed_image.shape[1]:
+        if sum(passed_image[x][y + 1]) == 0:  # if black
             bfs_arr.insert(0, [x, y + 1, color])
-            image[x][y + 1] = color
+            passed_image[x][y + 1] = color
             painted_pixels -= 1
             check_min_max_height_width(x, y + 1)
 
-    if x + 1 < image.shape[0] and y + 1 < image.shape[1]:
-        if sum(image[x + 1][y + 1]) == 0:  # if black
+    if x + 1 < passed_image.shape[0] and y + 1 < passed_image.shape[1]:
+        if sum(passed_image[x + 1][y + 1]) == 0:  # if black
             bfs_arr.insert(0, [x + 1, y + 1, color])
-            image[x + 1][y + 1] = color
+            passed_image[x + 1][y + 1] = color
             painted_pixels -= 1
             check_min_max_height_width(x + 1, y + 1)
 
-    if x + 1 < image.shape[0] and y - 1 >= 0:
-        if sum(image[x + 1][y - 1]) == 0:  # if black
+    if x + 1 < passed_image.shape[0] and y - 1 >= 0:
+        if sum(passed_image[x + 1][y - 1]) == 0:  # if black
             bfs_arr.insert(0, [x + 1, y - 1, color])
-            image[x + 1][y - 1] = color
+            passed_image[x + 1][y - 1] = color
             painted_pixels -= 1
             check_min_max_height_width(x + 1, y - 1)
 
     if y - 1 >= 0:
-        if sum(image[x][y - 1]) == 0:  # if black
+        if sum(passed_image[x][y - 1]) == 0:  # if black
             bfs_arr.insert(0, [x, y - 1, color])
-            image[x][y - 1] = color
+            passed_image[x][y - 1] = color
             painted_pixels -= 1
             check_min_max_height_width(x, y - 1)
 
     if x - 1 >= 0:
-        if sum(image[x - 1][y]) == 0:  # if black
+        if sum(passed_image[x - 1][y]) == 0:  # if black
             bfs_arr.insert(0, [x - 1, y, color])
-            image[x - 1][y] = color
+            passed_image[x - 1][y] = color
             painted_pixels -= 1
             check_min_max_height_width(x - 1, y)
 
     if x - 1 >= 0 and y - 1 >= 0:
-        if sum(image[x - 1][y - 1]) == 0:  # if black
+        if sum(passed_image[x - 1][y - 1]) == 0:  # if black
             bfs_arr.insert(0, [x - 1, y - 1, color])
-            image[x - 1][y - 1] = color
+            passed_image[x - 1][y - 1] = color
             painted_pixels -= 1
             check_min_max_height_width(x - 1, y - 1)
 
-    if x - 1 >= 0 and y + 1 < image.shape[1]:
-        if sum(image[x - 1][y + 1]) == 0:  # if black
+    if x - 1 >= 0 and y + 1 < passed_image.shape[1]:
+        if sum(passed_image[x - 1][y + 1]) == 0:  # if black
             bfs_arr.insert(0, [x - 1, y + 1, color])
-            image[x - 1][y + 1] = color
+            passed_image[x - 1][y + 1] = color
             painted_pixels -= 1
             check_min_max_height_width(x - 1, y + 1)
 
@@ -93,9 +99,11 @@ def bfs(x, y, color):
     bfs_arr.pop()
 
 
-def s(passed_image):
-    global painted_pixels, bfs_arr, min_height, max_height, min_width, max_width
+def draw_coins(passed_image):
+    global painted_pixels, bfs_arr, min_height, max_height, min_width, max_width, CHOSE_PAINTED_PIXELS, centers, center, COIN_RADIUS
+    image_copy = passed_image.copy()
 
+    # Step 1
     for row in range(len(passed_image)):
         for col in range(len(passed_image[row])):
 
@@ -103,7 +111,7 @@ def s(passed_image):
                 # started = True
                 rgb = [random.randrange(255), random.randrange(255), random.randrange(254)]
                 white = [255, 255, 255]
-                painted_pixels = 150
+                painted_pixels = CHOSE_PAINTED_PIXELS
                 passed_image[row][col] = white
                 bfs_arr = [[row, col, white]]
 
@@ -130,22 +138,15 @@ def s(passed_image):
                 # return
 
                 while len(bfs_arr) > 0 and painted_pixels > 0:
-                    bfs(bfs_arr[len(bfs_arr)-1][0], bfs_arr[len(bfs_arr)-1][1], rgb)
+                    bfs(passed_image, bfs_arr[len(bfs_arr)-1][0], bfs_arr[len(bfs_arr)-1][1], rgb)
 
                 # if painted_pixels < painted_pixels_copy * 0.1:
                 if painted_pixels == 0:
                     center.append([min_height, max_height, min_width, max_width])
 
-
-if __name__ == '__main__':
-    image = cv2.imread('map.PNG')
-    image_copy = image.copy()
-    # image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    print(image.shape)
-    s(image)
-    print("Done painting image")
-    print(len(center))
-    coins_num = 0
+    # Step 2
+    #  Might put this in new function
+    # coins_num = 0
     for i in center:
         row_y = int((i[0] + i[1]) / 2)
         col_x = int((i[2] + i[3]) / 2)
@@ -154,25 +155,91 @@ if __name__ == '__main__':
 
         added = []
         coin_outside = False
-        r = 5
-        for ball_y in range(-r, r):
-            for ball_x in range(-r, r):
-                if pow(ball_y, 2) + pow(ball_x, 2) <= r \
+        for ball_y in range(-COIN_RADIUS, COIN_RADIUS):
+            for ball_x in range(-COIN_RADIUS, COIN_RADIUS):
+                if pow(ball_y, 2) + pow(ball_x, 2) <= COIN_RADIUS \
                         and image_copy.shape[0] - 1 > row_y + ball_y >= 0 \
                         and image_copy.shape[1] - 1 > col_x + ball_x >= 0:
                     if sum(image_copy[row_y + ball_y][col_x + ball_x]) == 0:
-                        added.append([row_y+ball_y, col_x+ball_x])
+                        added.append([row_y + ball_y, col_x + ball_x])
                     else:
                         coin_outside = True
+
         # Detect and delete coins that got sliced. only perfect coins!
         if coin_outside is False:
-            coins_num += 1
             for pixel in added:
                 image_copy[pixel[0]][pixel[1]] = [255, 0, 0]
+                if row_y == pixel[0] and col_x == pixel[1]:
+                    centers.append([row_y, col_x])
 
-    print(coins_num)
+    # print(coins_num)
+    return image_copy
+
+
+def draw_lines(passed_image):
+    global centers, LINE_COLOR
+    sorted_centers = sort_centers()
+    for i in range(len(sorted_centers) - 1):
+        image_copy = og_image.copy()
+        cv2.line(image_copy, (sorted_centers[i][1], sorted_centers[i][0]),
+                 (sorted_centers[i + 1][1], sorted_centers[i + 1][0]), tuple(LINE_COLOR), 1)
+
+        xs = [sorted_centers[i][1], sorted_centers[i+1][1]]
+        ys = [sorted_centers[i][0], sorted_centers[i+1][0]]
+        color_ok = True
+        for y in range(min(ys), max(ys)+1):
+            if color_ok:
+                for x in range(min(xs), max(xs)+1):
+                    # print(og_image[y][x], np.asarray(LINE_COLOR))
+                    if np.array_equal(image_copy[y][x], np.asarray(LINE_COLOR)):
+                        if sum(og_image[y][x]) != 0:
+                            color_ok = False
+
+        if color_ok:
+            cv2.line(passed_image, (sorted_centers[i][1], sorted_centers[i][0]),
+                     (sorted_centers[i + 1][1], sorted_centers[i + 1][0]), tuple(LINE_COLOR), 1)
+    return passed_image
+
+
+def sort_centers():
+    global centers
+    sorted_arr = [centers[0]]
+    chosen = sorted_arr[0]
+    centers.pop()
+    first_iter = True
+    while len(centers) > 0:
+        min_distance = sys.maxsize
+        remember_index = 0
+        for i in range(len(centers)):
+            euc_distance = distance.euclidean([chosen[1], chosen[0]], [centers[i][1], centers[i][0]])
+            if euc_distance < min_distance:
+                min_distance = euc_distance
+                remember_index = i
+        sorted_arr.append(centers[remember_index])
+        chosen = centers[remember_index]
+        if first_iter:
+            first_iter = False
+        else:
+            centers = centers[:remember_index] + centers[remember_index + 1:]
+
+    return sorted_arr
+
+
+if __name__ == '__main__':
+    og_image = cv2.imread('map.PNG')
+
+    # image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    print(og_image.shape)
+    coins_image = draw_coins(og_image.copy())
+    print("Done painting image")
+    print("Number of perfect coins: " + str(len(center)))
+    lines_coins_image = draw_lines(coins_image.copy())
+
     # cv2.imwrite("food.png", image_copy)
-    plt.figure(figsize=[50, 20])
-    plt.subplot(131)
-    plt.imshow(image_copy, cmap='gray')
+    plt.figure(figsize=[15, 7])
+    # plt.subplot(131)
+    plt.imshow(lines_coins_image)
+    # plt.imshow(og_image, cmap='gray')
     plt.show()
+
+
---
+++ tkinter_tries.py
@@ -0,0 +1,26 @@
+from tkinter import *
+
+# pip install pillow
+from PIL import Image, ImageTk
+
+
+class Window(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.pack(fill=BOTH, expand=1)
+
+        load = Image.open("map.PNG")
+        render = ImageTk.PhotoImage(load)
+        img = Label(self, image=render)
+        img.image = render
+        img.place(x=0, y=0)
+
+
+
+
+root = Tk()
+app = Window(root)
+root.wm_title("Tkinter window")
+root.geometry("1000x500")
+root.mainloop()
